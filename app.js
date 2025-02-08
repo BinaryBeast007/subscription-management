03a00a66609619fda0e37aea49904b036e1dc681
Abu Sayem <abusayem1299@gmail.com>
2025-02-09 02:11:11 +0600

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subcriptions.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -14,10 +15,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
     `Subscription Management System is running on http://localhost:${PORT}`,
   );
+  await connectToDatabase();
 });
 
 export default app;
