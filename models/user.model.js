08316e8ce0d1176ea15a44c221e40a78db75dc3e
Abu Sayem <abusayem1299@gmail.com>
2025-02-09 18:59:30 +0600

    feat: add user signup and signin funtionality

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -27,24 +26,6 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-
-// Pre-save hook to hash passwords before storing in the database
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next(); 
-
-  try {
-    const salt = await bcrypt.genSalt(10); 
-    this.password = await bcrypt.hash(this.password, salt);
-    next(); 
-  } catch (error) {
-    next(error);
-  }
-});
-
-// Method to compare passwords for authentication
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model("User", userSchema);
 
